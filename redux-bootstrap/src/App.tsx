f9bbed4410f848965024fed4cb6e11f4e0e2c1f7
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-07-06 23:55:00 +0600

    first commit

--- redux-bootstrap/src/App.tsx
+++ redux-bootstrap/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import type { RootState } from "./redux/store";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const dispatch = useAppDispatch();
+    const { count } = useAppSelector((state: RootState) => state.counter);
+    const handleIncrement = (amount: number) => {
+        dispatch(increment(amount));
+    };
+    const handleDecrement = () => {
+        dispatch(decrement());
+    };
+    return (
+        <div>
+            <h1>Counter with Redux</h1>
+            <button onClick={() => handleIncrement(1)}>Increment</button>
+            <button onClick={() => handleIncrement(5)}>Increment by 5</button>
+            <div>{count}</div>
+            <button onClick={handleDecrement}>Decrement</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
