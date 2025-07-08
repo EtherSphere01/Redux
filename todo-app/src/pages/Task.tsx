bbbbf84b845aa3fc34af8be1512cd0941c43946c
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-07-08 20:52:34 +0600

    Notes

--- todo-app/src/pages/Task.tsx
+++
@@ -1,9 +0,0 @@
-import { selectTask } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
-
-const Task = () => {
-    const tasks = useAppSelector(selectTask);
-    return <div>this is task</div>;
-};
-
-export default Task;
--- todo-app/src/pages/User.tsx
+++
@@ -1,7 +0,0 @@
-
-
-const User = () => {
-    return <div>this is user</div>;
-};
-
-export default User;
